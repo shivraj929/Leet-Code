8871a53056cf4bf6b9a7271f100d876d7b007158
shivraj929 <panwarshivrajsingh99@gmail.com>
2026-05-04 00:58:42 +0530

    Add files via upload

---
+++ question3.cpp
@@ -0,0 +1,57 @@
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+ 
+
+// Follow up: Could you solve it without converting the integer to a string?
+
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return 0;
+        }
+        int origanal=x;
+        long long temp=0;
+        while(x>0){
+                temp=temp*10+(x%10);
+                x=x/10;
+        }
+        return origanal==temp;
+    }
+};
+int main(){
+    int x;
+    cout<<"Enter number to check Palindrome :-";
+    cin>>x;
+    Solution obj;
+    if(obj.isPalindrome(x)){
+        cout<<"True";
+    }
+    else{
+        cout<<"False";
+    }
+}
